2aa1692439bd20e0d23cb1e8126efe671fd0d5ec
Jinxu Zheng <1710198939@qq.com>
2023-10-22 20:07:59 -0500

    lastest version

--- mobileapp/components/screens/notes/DailyNotes.js
+++ mobileapp/components/screens/notes/DailyNotes.js
@@ -1,16 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, StyleSheet, FlatList, Platform } from 'react-native';
+import { View, StyleSheet, FlatList, TextInput } from 'react-native';
 import { FAB } from 'react-native-paper'; // Import FAB (Floating Action Button)
 import { GlobalContext } from "../../../context/GlobalContext";
 import { getUserNotes } from '../../../network/useNoteService';
 import ShowItem from './NodeItem';
+import { IconButton, MD3Colors, Divider } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function DailyNotes({ navigation }) {
   const { globalState, setGlobalState } = useContext(GlobalContext);
   const [dailyNotes, setDailyData] = useState({});
+  const [searchText, setSearhText] = useState("");
+
   const userId = globalState.userInfo.id;
   const fetchNoteData = async () => {
-    const noteData = await getUserNotes(globalState.userInfo.token,userId);
+    const noteData = await getUserNotes(globalState.userInfo.token, userId);
     return noteData;
   };
 
@@ -18,6 +22,32 @@ export default function DailyNotes({ navigation }) {
     const noteData = await fetchNoteData();
     setGlobalState({ ...globalState, DailyNotes: noteData.data });
   };
+
+  const handleSearchText = (text) => {
+    setSearhText(text);
+    const filteredData = filterData(globalState.DailyNotes, text);
+    setDailyData(filteredData);
+  }
+
+  const filterData = (data, searchText) => {
+    return data.filter((item) => {
+      return item.header.toLowerCase().includes(searchText.toLowerCase());
+    });
+  }
+
+  const handleAsc = () => {
+    const sortedData = [...globalState.DailyNotes];
+    sortedData.sort((a, b) => a.header.localeCompare(b.header));
+    const filteredData = filterData(sortedData, searchText);
+    setDailyData(filteredData);
+  }
+
+  const handleDesc = () => {
+    const sortedData = [...globalState.DailyNotes];
+    sortedData.sort((a, b) => b.header.localeCompare(a.header));
+    const filteredData = filterData(sortedData, searchText);
+    setDailyData(filteredData);
+  }
 
   useEffect(() => {
     setDailyData(globalState.DailyNotes);
@@ -33,6 +63,45 @@ export default function DailyNotes({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <View style={{
+        position: "relative",
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 5,
+      }}>
+        <TextInput
+          placeholder="Search your notes..."
+          style={{
+            flex: 1,
+            paddingVertical: 10,
+            paddingHorizontal: 10,
+            paddingLeft: 40,
+          }}
+          onChangeText={handleSearchText}
+          value={searchText}
+        />
+        <Icon
+          name="search"
+          size={20}
+          color="#333"
+          style={{ position: "absolute", top: 10, left: 10, zIndex: 1 }}
+        />
+        <IconButton
+          icon="sort-alphabetical-ascending"
+          iconColor={MD3Colors.error50}
+          size={30}
+          style={{ position: "absolute", top: -10, right: 40, zIndex: 1 }}
+          onPress={handleAsc}
+        />
+        <IconButton
+          icon="sort-alphabetical-descending"
+          iconColor={MD3Colors.error50}
+          size={30}
+          style={{ position: "absolute", top: -10, right: 5, zIndex: 1 }}
+          onPress={handleDesc}
+        />
+      </View>
       <FlatList
         data={dailyNotes}
         keyExtractor={(item) => item._id}
